208d1c82c08eb2beca7d490675e8649c9501166b
Isha Kumari <isha02740@gmail.com>
2024-06-03 12:43:33 +0530

    first commit

---
+++ tutorial.js
@@ -0,0 +1,8 @@
+"use strict";
+// let id:number=5;
+// let company:string="ABC";
+// let isPublished:boolean=true;
+const signContract = (employee) => {
+    console.log("Contract signed by  " + employee.name + " with email: " + employee.email);
+};
+signContract({ name: "dfvf", creditScore: 500, id: 6, email: "dfvf@gmail.com" });
---
+++ tutorial.ts
@@ -0,0 +1,49 @@
+// let id:number=5;
+// let company:string="ABC";
+// let isPublished:boolean=true;
+
+// let ids:number[]=[1,2,3,4,5];
+// let x:any="abc";
+// let xArr:any[]=[true,"rst",0];
+
+// let a:string="hello"; 
+// let b:string="world";
+
+// const concatenateValues=(a:string,b:string)=>{
+//     return a+b;
+// }
+// console.log(concatenateValues("Hello","World"));
+// console.log(concatenateValues("5","10"));
+
+
+
+
+// INTERFACES AND TYPES
+// interface UserInterface {
+//     greet(message: string): void;
+// }
+
+// const User: UserInterface = {
+//     greet(message){
+//         console.log(message);
+//     },
+// };
+// User.greet("Hello");
+
+
+// interface BusinessPartner {
+//     name:string;
+//     creditScore:number;
+// }
+// interface UserIdentity{
+//     id:number;
+//     email:string;
+// }
+// type Employee=BusinessPartner & UserIdentity
+
+// const signContract=(employee:Employee):void=>
+
+// {
+//     console.log("Contract signed by  " + employee.name + " with email: "  + employee.email)
+// }
+// signContract({name:"dfvf",creditScore:500,id:6,email:"dfvf@gmail.com"})
